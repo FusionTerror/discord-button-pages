a88dcc41d6b4f6df08ee427ba4d87c16fdd1859b
Fusion Terror <64173400+FusionTerror@users.noreply.github.com>
2021-06-23 16:16:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Discord-ClickPages 📖
+##### *Interactive Discord embed buttons. Allows for pages to be created with Discord embeds in Discord.js v12!*
+##### 💖 ~ Support: [Fusion Terror's YouTube](https://www.youtube.com/channel/UCjTvZBc6GFbYkVs9rGWJLbA), [Fusion Terror's Patreon](https://www.patreon.com/fusionterror)
+##### 🙋‍♂ ~ Help: [Discord Server](https://discord.gg/QJyTkNxVrX)
+---
+&nbsp;
+## ✅ ~ Installation
+```
+npm i discord-clickpages
+```
+&nbsp;
+## ⚙️ ~ Setup
+ ### You **must** have [discord-buttons](https://www.npmjs.com/package/discord-buttons) installed in your project. From there you will need to pass in the client constructor from Discord.js into Discord--Buttons as shown below:
+```js
+const Discord = require('discord.js'); //Requiring Discord.js module.
+const client = new Discord.Client(); //Creating and assigning the Discord.js Client constructor.
+const DiscordButtons = require('discord-buttons');
+DiscordButtons(client);
+```
+&nbsp;
+## 📝 ~ Features
+
+- Interactive buttons.
+- Swap between embeds quickly and effortlessly.
+- Customizable button emojis.
+- Discord.js 12 Support
+- Discord Server Support
+&nbsp;
+
+## 💡 ~ Example
+```js
+const Discord = require('discord.js'); //Requiring Discord.js module.
+const client = new Discord.Client(); //Creating and assigning the Discord.js Client constructor.
+const DiscordButtons = require('discord-buttons');
+const ClickPages = require('discord-clickpages');
+DiscordButtons(client);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!test') {
+    const embed1 = new Discord.MessageEmbed()
+        .setTitle('Embed #1')
+        .setColor('RED');
+        
+    const embed2 = new Discord.MessageEmbed()
+        .setTitle('Embed #2')
+        .setColor('YELLOW');
+        
+    const embed3 = new Discord.MessageEmbed()
+        .setTitle('Embed #3')
+        .setColor('BLUE');
+    
+    const embedPages = [embed1, embed2, embed3];
+    ClickPages.createPages(client, msg, embedPages, 60 * 1000, "red", "👉", "👈", "❌");
+  }
+});
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+    createPages: async (client, message, embeds, duration, buttonStyle, rightEmoji, leftEmoji, cancelEmoji) => {
+        if (!["red", "green", "blurple"].includes(buttonStyle)) throw new TypeError(`Button style provided is not valid.`);
+        if (!rightEmoji) throw new TypeError(`An emoji to go to the next page was not provided.`);
+        if (!leftEmoji) throw new TypeError(`An emoji to go to the previous page was not provided.`);
+        if (!leftEmoji) throw new TypeError(`An emoji to go cancel the embed page was not provided.`);
+        const { MessageButton, MessageActionRow } = require('discord-buttons'); //Requiring MessageButton.
+        const buttonStartTime = Date.now(); //Current time of when buttons were made.
+        let currentPage = 0;
+        const nextPageButton = new MessageButton()
+            .setLabel("")
+            .setStyle(buttonStyle)
+            .setEmoji(rightEmoji)
+            .setID('next-page');
+
+        const backPageButton = new MessageButton()
+            .setLabel("")
+            .setStyle(buttonStyle)
+            .setEmoji(leftEmoji)
+            .setID('back-page');
+
+        const deletePageButton = new MessageButton()
+            .setLabel("")
+            .setStyle(buttonStyle)
+            .setEmoji(cancelEmoji)
+            .setID('cancel-page');
+
+        const interactivePages = new MessageActionRow()
+            .addComponent(backPageButton)
+            .addComponent(deletePageButton)
+            .addComponent(nextPageButton);
+
+        const msg = await message.channel.send({ components: [interactivePages], embed: embeds[0] });
+        client.on('clickButton', async button => {
+            if (message.author !== button.clicker.user || Date.now() - buttonStartTime >= duration || button.message.id == msg.id) return;
+            if (button.id == 'next-page') {
+                (currentPage + 1 == embeds.length ? currentPage = 0 : currentPage += 1);
+                msg.edit({ embed: embeds[currentPage], components: [interactivePages] });
+                button.defer(true);
+            } else if (button.id == 'back-page') {
+                (currentPage - 1 < 0 ? currentPage = emeds.length -1 : currentPage -= 1);
+                msg.edit({ embed: embeds[currentPage], components: [interactivePages] });
+                button.defer(true);
+            } else if (button.id == 'cancel-page') {
+                await msg.delete()
+                    .catch(err => console.error(err));
+            }
+        });
+    },
+};
